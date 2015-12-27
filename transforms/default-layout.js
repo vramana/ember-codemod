8d89451e994a63034f40c5c9452ff598580d1109
Ramana Venkata <idlike2dream@gmail.com>
2015-12-28 00:01:20 +0530

    Change the implementation of default-layout transform

--- transforms/default-layout.js
+++ transforms/default-layout.js
@@ -1,17 +1,48 @@
 export default function (file, api) {
   const j = api.jscodeshift
 
-  return j(file.source)
-    .find(j.ObjectExpression)
-    .replaceWith(p => {
-    	const { properties } = p.node;
-     	Object.keys(properties).forEach(property => {
-        if (properties[property].key.name === 'defaultLayout') {
-			     properties[property].key.name = 'layout'
-        }
-      })
+  const isProperty = p => {
+    return (
+      p.parent.node.type === 'Property' &&
+      p.parent.node.key.type === 'Identifier' &&
+      p.parent.node.key.name === 'defaultLayout'
+    )
+  }
 
-    	return p.node;
-  	})
+  const checkCallee = node => {
+    const types = (
+      node.type === 'MemberExpression' &&
+      node.object.type === 'MemberExpression' &&
+      node.object.object.type === 'Identifier' &&
+      node.object.property.type === 'Identifier' &&
+      node.property.type === 'Identifier'
+    )
+
+    const identifiers = (
+      node.object.object.name === 'Ember' &&
+      node.object.property.name === 'Component' &&
+      node.property.name === 'extend'
+    )
+
+    return types && identifiers
+  }
+
+  const isArgument = p => {
+    if (p.parent.parent.parent.node.type === 'CallExpression') {
+      const call = p.parent.parent.parent.node
+      return checkCallee(call.callee)
+    }
+  }
+
+  const replaceDefaultLayout = p => {
+    p.node.name = 'layout'
+    return p.node
+  }
+
+  return j(file.source)
+    .find(j.Identifier, { name: 'defaultLayout' })
+    .filter(isProperty)
+    .filter(isArgument)
+    .replaceWith(replaceDefaultLayout)
     .toSource();
 }
